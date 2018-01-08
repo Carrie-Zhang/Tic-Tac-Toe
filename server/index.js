a96f00d7ebb2388f2011e542213ba92c0ff27227
Carrie Zhang <carrie.zhang8206@gmail.com>
2018-01-08 15:10:32 -0800

    implement localhost:3500

--- server/index.js
+++ server/index.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+var app = express();
+
+app.get('/', (req, res) => { 
+  res.send('Hello!'); 
+});
+
+app.listen(3500, () => console.log('Tic Tac Toe Game is listening on port 3500'));
